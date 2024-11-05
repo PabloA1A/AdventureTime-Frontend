c86e8970ea17b883f8f446c2384519fcbc083069
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-05 21:19:21 +0100

    modified HomeView for the new

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup>
-
+import Carrusel from '@/components/Carrusel.vue'
+import Event from '@/components/Event.vue'
 </script>
 
 <template>
-    <h1>Home</h1>
+    <div class="w-full bg-background-primary bg-center bg-no-repeat bg-fixed bg-cover pt-10">
+    <Carrusel />
+    <div class="w-full flex justify-center align-middle pt-10 pb-10 bg-light/30 backdrop-blur">
+      <span class="text-3xl font-bold">Available events</span>
+    </div>
+    <Event />
+  </div>
 </template>
 
 <style></style>
