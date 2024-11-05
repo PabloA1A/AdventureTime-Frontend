88d18a33086ade12f2b44469b39f667e06cbd216
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-05 16:30:47 +0100

    created RegisterView with all its logic

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useAuthStore } from "@/stores/auth";
+import Register from "../components/Register.vue"
+
+const store = useAuthStore()
+
+</script>
+
+<template>
+   <Register />
+</template>
+
+<style>
+</style>
